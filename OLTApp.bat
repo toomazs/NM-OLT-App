6bc528a9c2b9b2cd6da45aeecb5bfa2e39c19d28
Eduardo Tomaz <eduardotoomaz@outlook.com>
2025-04-20 21:41:13 -0300

    v1.4.0

--- OLTApp.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-title OLT Manager
-set FX_PATH=%~dp0lib\javafx-sdk-24\lib
-java --module-path "%FX_PATH%" --add-modules javafx.controls,javafx.fxml,javafx.graphics --enable-native-access=javafx.graphics -jar OLTApp.jar
-pause
